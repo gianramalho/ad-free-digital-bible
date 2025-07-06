666785502030533b260bd64b6d4061bb742ede2c
Gian Ramalho <gian15249@gmail.com>
2025-07-06 13:53:28 -0400

    Ajustes na versão 1.0.1 para o SDK Android 15 (nível 35 da API)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,12 +2,11 @@ import "@/theme/global.css";
 import { View } from "react-native"
 import { Stack } from "expo-router"
 import { StatusBar } from "expo-status-bar"
-import { SQLiteProvider } from "expo-sqlite";
-import { initializeDatabase } from "@/database/initializeDatabase"
 import { LoadingProvider } from "@/contexts/loadingContext";
 import { useKeepAwake } from "expo-keep-awake";
 import { ToastProvider } from "react-native-toast-notifications";
 import { ThemeProvider } from "@/providers/themeProvider";
+import { SQLiteProvider } from "expo-sqlite";
 
 export default function Layout() {
     useKeepAwake();
@@ -16,7 +15,7 @@ export default function Layout() {
         <ThemeProvider>
             <LoadingProvider>
                 <ToastProvider>
-                    <SQLiteProvider databaseName="database" onInit={initializeDatabase}>
+                    <SQLiteProvider databaseName="database" assetSource={{ assetId: require('../assets/mydb.db') }}>
                         <View className="flex-1" >
                             <StatusBar style="auto" backgroundColor="transparent" translucent />
                             <Stack screenOptions={{
